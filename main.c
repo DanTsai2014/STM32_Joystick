3461ceab19005e37abe5f0229ae93faeb204022a
DanTsai2014 <akitotsai@hotmail.com.tw>
2016-01-20 16:36:56 +0800

    AD of multiple channel

--- main.c
+++ main.c
@@ -83,12 +83,45 @@ void adc_init() {
 	GPIO_InitTypeDef GPIO_initStructre; //Structure for analog input pin
 	//Clock configuration
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE); //The ADC1 is connected the APB2 peripheral bus thus we will use its clock source
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2, ENABLE); //The ADC2 is connected the APB2 peripheral bus thus we will use its clock source
+	//RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2, ENABLE); //The ADC2 is connected the APB2 peripheral bus thus we will use its clock source
 	RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN, ENABLE); //Clock for the ADC port!! Do not forget about this one ;)
 	//Analog pin configuration
 	GPIO_StructInit(&GPIO_initStructre);
-	GPIO_initStructre.GPIO_Pin = GPIO_Pin_0; //The channel 10 is connected to PC0
-	GPIO_initStructre.GPIO_Pin = GPIO_Pin_1; //The channel 11 is connected to PC1
+	GPIO_initStructre.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1; //The channel 10 is connected to PC0
+	//GPIO_initStructre.GPIO_Pin = GPIO_Pin_1; //The channel 11 is connected to PC1
+	GPIO_initStructre.GPIO_Mode = GPIO_Mode_AN; //The PC0 pin is configured in analog mode
+	GPIO_initStructre.GPIO_PuPd = GPIO_PuPd_NOPULL; //We don't need any pull up or pull down
+	GPIO_Init(GPIOC, &GPIO_initStructre); //Affecting the port with the initialization structure configuration
+	//ADC structure configuration
+	ADC_DeInit();
+	ADC_init_structure.ADC_DataAlign = ADC_DataAlign_Right; //data converted will be shifted to right
+	ADC_init_structure.ADC_Resolution = ADC_Resolution_12b; //Input voltage is converted into a 12bit number giving a maximum value of 4096
+	ADC_init_structure.ADC_ContinuousConvMode = ENABLE; //the conversion is continuous, the input data is converted more than once
+	ADC_init_structure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1; // conversion is synchronous with TIM1 and CC1 (actually I'm not sure about this one :/)
+	ADC_init_structure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None; //no trigger for conversion
+	ADC_init_structure.ADC_NbrOfConversion = 2; //Number of used ADC channels
+	ADC_init_structure.ADC_ScanConvMode = ENABLE; //The scan is configured in one channel
+	ADC_Init(ADC1, &ADC_init_structure); //Initialize ADC with the previous configuration
+	//ADC_Init(ADC2, &ADC_init_structure);
+	//Enable ADC conversion
+	ADC_Cmd(ADC1, ENABLE);
+	//ADC_Cmd(ADC2, ENABLE);
+	//Select the channel to be read from
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_144Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 2, ADC_SampleTime_144Cycles);
+}
+/*
+void adc2_init() {
+
+	ADC_InitTypeDef ADC_init_structure; //Structure for adc confguration
+	GPIO_InitTypeDef GPIO_initStructre; //Structure for analog input pin
+	//Clock configuration
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2, ENABLE); //The ADC1 is connected the APB2 peripheral bus thus we will use its clock source
+	RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN, ENABLE); //Clock for the ADC port!! Do not forget about this one ;)
+	//Analog pin configuration
+	GPIO_StructInit(&GPIO_initStructre);
+	GPIO_initStructre.GPIO_Pin = GPIO_Pin_1; //The channel 10 is connected to PC0
+	//GPIO_initStructre.GPIO_Pin = GPIO_Pin_1; //The channel 11 is connected to PC1
 	GPIO_initStructre.GPIO_Mode = GPIO_Mode_AN; //The PC0 pin is configured in analog mode
 	GPIO_initStructre.GPIO_PuPd = GPIO_PuPd_NOPULL; //We don't need any pull up or pull down
 	GPIO_Init(GPIOC, &GPIO_initStructre); //Affecting the port with the initialization structure configuration
@@ -102,15 +135,15 @@ void adc_init() {
 	ADC_init_structure.ADC_NbrOfConversion = 1; //I think this one is clear :p
 	ADC_init_structure.ADC_ScanConvMode = DISABLE; //The scan is configured in one channel
 	ADC_Init(ADC1, &ADC_init_structure); //Initialize ADC with the previous configuration
-	ADC_Init(ADC2, &ADC_init_structure);
+	//ADC_Init(ADC2, &ADC_init_structure);
 	//Enable ADC conversion
 	ADC_Cmd(ADC1, ENABLE);
-	ADC_Cmd(ADC2, ENABLE);
+	//ADC_Cmd(ADC2, ENABLE);
 	//Select the channel to be read from
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_144Cycles);
-	ADC_REgularChannelConfig(ADC2, ADC_Channel_11, 1, ADC_SampleTime_144Cycles);
+	//ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 2, ADC_SampleTime_144Cycles);
 }
-
+*/
 
 void vApplicationTickHook(void) {
 }
@@ -167,18 +200,19 @@ u16 readADC1(u8 channel) {
 	 while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));//Processing the conversion
 	 return ADC_GetConversionValue(ADC1); //Return the converted data
 }
-
+/*
 u16 readADC2(u8 channel) {
 	ADC_SoftwareStartConv(ADC2);
 	while(!ADC_GetFlagStatus(ADC2, ADC_FLAG_EOC));
 	return ADC_GetConversionValue(ADC2);
 }
-
+*/
 int main(void) {
 
 		uint8_t ret = pdFALSE;
                 int i;
-                char buff [] = "";
+                char buff_x [] = "";
+                char buff_y [] = "";
     /*init.*/
 		NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 		init_USART3(9600);
@@ -194,9 +228,12 @@ int main(void) {
                 
                 while (1)
                 {
-                   sprintf (buff, "ADC_x: %d \n\r ADC_y: %d \n\r", readADC1(16), readADC2(16));
-                   Usart3_Printf(buff); // send string to USART3
-                   for(i=0; i<3000000; i++); // delay
+                   sprintf (buff_x, "ADC: %d\n\r", readADC1(16));
+                   //sprintf (buff_y, "ADC_y: %d \n\r", readADC1(16));
+                   Usart3_Printf(buff_x); // send string to USART3
+                   for(i=0; i<30000000; i++);
+                   //Usart3_Printf(buff_y);
+                   //for(i=0; i<3000000; i++); // delay
                 }
         /*unit testing.*/
         if(unit_tests_task()){ /*unit tests not pass. */
@@ -211,7 +248,7 @@ int main(void) {
 
 		/*create the task. */         
         printf("Task creating...........\r\n");
-		    ret = xTaskCreate(neural_task, "neural PID update task", 8192 /*configMINIMAL_STACK_SIZE*/, NULL, 2, NULL);
+		ret = xTaskCreate(neural_task, "neural PID update task", 8192 /*configMINIMAL_STACK_SIZE*/, NULL, 2, NULL);
         ret &= xTaskCreate(receive_task, "receive command task", 1024 /*configMINIMAL_STACK_SIZE*/, NULL, 3, NULL);
         ret &= xTaskCreate(send_data_task, "send data task", 1024 /*configMINIMAL_STACK_SIZE*/, NULL, 1, NULL);
 		//ret &= xTaskCreate(send_out_task, "send out information task", 1024 /*configMINIMAL_STACK_SIZE*/, NULL, 1, NULL);
